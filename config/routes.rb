4083cc605194faf15c590a61cf11d3fb53f639d8
Alexander Groleau <alex@teampalmtree.com>
2015-02-23 03:13:20 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-get 'backlogs',
-	:controller => :backlogs, :action => :index,
-	:as => :backlogs
+get 'backlogs', :to => 'backlogs#index'
