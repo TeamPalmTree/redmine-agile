d9e9d70ff5f858cd6c3b14355f76c8bff73131ab
Alexander Groleau <alex@teampalmtree.com>
2015-02-23 14:50:50 -0500

    trying issue pull

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-get '/projects/:project_id/agile', :to => 'agile#index'
-get '/projects/:project_id/agile/backlogs', :to => 'agile#index'
+get '/projects/:project_id/agile/(:tab)', :to => 'agile#index'
