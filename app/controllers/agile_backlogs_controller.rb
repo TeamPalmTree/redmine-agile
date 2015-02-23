d9b61d55e2790b1619f323309db1bfbf01f96b19
Alexander Groleau <alex@teampalmtree.com>
2015-02-23 02:18:16 -0500

    and the menu is active?

--- app/controllers/agile_backlogs_controller.rb
+++ app/controllers/agile_backlogs_controller.rb
@@ -1,12 +1,14 @@
 class AgileBacklogsController < ApplicationController
 	unloadable
-	
+
+	before_filter :find_project, :authorize, :only => :index
+
 	def index
 	end
 
-	def find_project
-    # @project variable must be set before calling the authorize filter
-    @project = Project.find(params[:project_id])
-  end
+	 def find_project
+		# @project variable must be set before calling the authorize filter
+		@project = Project.find(params[:project_id])
+	end
 
 end
