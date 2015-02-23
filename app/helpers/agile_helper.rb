685fdaae23937755462f935ee784e5227e3b277f
Alexander Groleau <alex@teampalmtree.com>
2015-02-23 04:32:34 -0500

    translations omg

--- app/helpers/agile_helper.rb
+++ app/helpers/agile_helper.rb
@@ -1,9 +1,9 @@
 module AgileHelper
 	def agile_tabs
 	    tabs = [
-			{:name => 'backlogs', :partial => 'agile/backlogs', :label => 'Backlogs'},
-			{:name => 'scrum', :partial => 'agile/backlogs', :label => 'Scrum'},
-			{:name => 'reports', :partial => 'agile/backlogs', :label => 'Reports'}
+			{:name => 'backlogs', :partial => 'agile/backlogs', :label => :label_backlog_plural},
+			{:name => 'scrum', :partial => 'agile/backlogs', :label => :label_scrum_plural},
+			{:name => 'reports', :partial => 'agile/backlogs', :label => :label_report_plural}
 		]
 	end
 end
