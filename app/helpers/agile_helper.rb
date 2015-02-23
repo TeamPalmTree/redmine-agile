cae9223fd97618fa9d7a0ffb7d8e90537e8a68c3
Alexander Groleau <alex@teampalmtree.com>
2015-02-23 04:56:11 -0500

    tab name changes

--- app/helpers/agile_helper.rb
+++ app/helpers/agile_helper.rb
@@ -1,7 +1,7 @@
 module AgileHelper
 	def agile_tabs
 	    tabs = [
-			{:name => 'boards', :partial => 'agile/backlogs', :label => :label_board_plural},
+			{:name => 'sprints', :partial => 'agile/backlogs', :label => :label_sprint_plural},
 			{:name => 'backlogs', :partial => 'agile/backlogs', :label => :label_backlog_plural},
 			{:name => 'graphs', :partial => 'agile/backlogs', :label => :label_graph_plural}
 		]
