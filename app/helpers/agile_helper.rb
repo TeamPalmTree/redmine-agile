3bdaaae3d52e1b3c4bbcbb15958e90de12052ab8
Alexander Groleau <alex@teampalmtree.com>
2015-02-23 05:08:39 -0500

    additional tab

--- app/helpers/agile_helper.rb
+++ app/helpers/agile_helper.rb
@@ -1,8 +1,9 @@
 module AgileHelper
 	def agile_tabs
 	    tabs = [
-			{:name => 'sprints', :partial => 'agile/backlogs', :label => :label_sprint_plural},
-			{:name => 'backlogs', :partial => 'agile/backlogs', :label => :label_backlog_plural},
+	    	{:name => 'backlogs', :partial => 'agile/backlogs', :label => :label_backlog_plural},
+	    	{:name => 'kanban_board', :partial => 'agile/backlogs', :label => :label_kanban_board},
+			{:name => 'scrum_boards', :partial => 'agile/backlogs', :label => :label_scrum_board_plural},
 			{:name => 'graphs', :partial => 'agile/backlogs', :label => :label_graph_plural}
 		]
 	end
