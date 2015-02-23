52efaee5d78adb184cbb6bcf8d138afa70e83401
Alexander Groleau <alex@teampalmtree.com>
2015-02-23 15:29:29 -0500

    trying issue pull

--- app/helpers/agile_helper.rb
+++ app/helpers/agile_helper.rb
@@ -1,9 +1,9 @@
 module AgileHelper
 	def agile_tabs
 	    tabs = [
-	    	{:name => 'backlogs', :partial => 'agile/backlogs', :label => :label_backlog_plural},
-	    	{:name => 'boards', :partial => 'agile/backlogs', :label => :label_board_plural},
-			{:name => 'graphs', :partial => 'agile/backlogs', :label => :label_graph_plural}
+	    	{:name => 'backlogs', :partial => 'agile/backlogs', :refresh => 'agile/backlogs/all', :label => :label_backlog_plural},
+	    	{:name => 'boards', :partial => 'agile/backlogs', :refresh => 'agile/backlogs/all', :label => :label_board_plural},
+			{:name => 'graphs', :partial => 'agile/backlogs', :refresh => 'agile/backlogs/all', :label => :label_graph_plural}
 		]
 	end
 end
