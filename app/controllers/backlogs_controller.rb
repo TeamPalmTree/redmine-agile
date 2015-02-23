438025161d48f8ef49f2176a0310a3f2b578229e
Alexander Groleau <alex@teampalmtree.com>
2015-02-23 03:36:41 -0500

    yay project menu :D

--- app/controllers/backlogs_controller.rb
+++ app/controllers/backlogs_controller.rb
@@ -1,7 +1,14 @@
 class BacklogsController < ApplicationController
   unloadable
 
-  def index
-  end
+	before_filter :find_project, :authorize, :only => :index
+
+	def index
+	end
+
+	def find_project
+		# @project variable must be set before calling the authorize filter
+		@project = Project.find(params[:project_id])
+	end
 
 end
