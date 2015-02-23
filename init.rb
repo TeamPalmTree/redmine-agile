4308548f478324fcf38a958c70cab85666312478
Alexander Groleau <alex@teampalmtree.com>
2015-02-23 03:05:59 -0500

    fixing

--- init.rb
+++ init.rb
@@ -7,6 +7,6 @@ Redmine::Plugin.register :redmnine_agile do
   version '0.0.1'
   url 'https://github.com/TeamPalmTree/redmine-agile'
   author_url 'https://github.com/awgneo'
-  permission :agile, { :agile => [:index] }, :public => true
+  permission :agile, { :backlogs => [:index] }, :public => true
   menu :project_menu, :agile, { :controller => 'backlogs', :action => 'index' }, :caption => 'Agile', :after => :activity, :param => :project_id
 end
