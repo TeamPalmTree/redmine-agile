ca9fc8a2e3db2e8758d64e2547572625029925a6
Alexander Groleau <alex@teampalmtree.com>
2015-02-23 22:50:36 -0500

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 get '/projects/:project_id/agile', :to => 'agile#index', as: 'agile_mode'
-get '/projects/:project_id/agile/backlogs', :to => 'agile#backlogs'
+get '/projects/:project_id/agile/backlogs', :to => 'agile#backlogs', as: 'agile_backlogs'
 get '/projects/:project_id/agile/board/:version', :to => 'agile#board', as: 'agile_board'
 get '/projects/:project_id/agile/graph/:graph', :to => 'agile#graph', as: 'agile_graph'
